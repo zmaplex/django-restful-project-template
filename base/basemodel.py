cb490c8eed272b011301d3bd454586abf95ebd40
zmaplex <zmaplex@gmail.com>
2023-08-18 20:37:57 +0800

    update name

--- base/basemodel.py
+++ base/basemodel.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    create_at = models.DateTimeField(
+    created_at = models.DateTimeField(
         auto_now_add=True, help_text="创建日期", verbose_name="创建日期"
     )
-    update_at = models.DateTimeField(
+    updated_at = models.DateTimeField(
         auto_now=True, help_text="更新日期", verbose_name="最后修改"
     )
 
